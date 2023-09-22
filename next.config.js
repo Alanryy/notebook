526e6ab6898c56844d88ca1465a7fdcc422b6314
Alanryy <hamzaouialae5@gmail.com>
2023-09-22 16:53:49 +0200

    change

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-
-}
-
-
-module.exports = nextConfig
+module.exports = {
+    exportPathMap: async function (defaultPathMap) {
+      // Define custom path mappings for static export
+      const customPathMap = {
+        // Add your custom path mappings here
+      };
+  
+      // Merge the default and custom path mappings
+      return Object.assign({}, defaultPathMap, customPathMap);
+    },
+  };
