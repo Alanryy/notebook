1f5be71cf4919332a62cbe0e56cb13ba075356af
Alanryy <hamzaouialae5@gmail.com>
2023-09-27 10:08:21 +0200

    change

--- next.config.js
+++ next.config.js
@@ -2,29 +2,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    output: 'export',
-    trailingSlash: true,
-    kipTrailingSlashRedirect: true,
-    distDir: 'dist',
-    // basePath: '/api',
-  async generateStaticParams() {
-    // Define how to generate static versions of dynamic routes
-    const dynamicParams = [
-      // Define the dynamic parameters for your route
-      { id: '1' },
-      { id: '2' },
-      // Add more dynamic parameters as needed
-    ];
-
-    // Create an array of static paths based on dynamic parameters
-    const paths = dynamicParams.map((params) => ({
-      params,
-      // Specify the page component for this route
-      page: '/api/notepages/[id]',
-    }));
-
-    return paths;
-  },
 }
  
 module.exports = nextConfig
