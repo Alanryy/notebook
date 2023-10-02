a0d3074408f7c18d74e8c92dfd84d033bfffbe4d
Alanryy <hamzaouialae5@gmail.com>
2023-10-02 22:14:57 +0200

    change

--- app/components/Category.tsx
+++ app/components/Category.tsx
@@ -11,12 +11,12 @@ interface Page {
 
 
 const Pagecat = async () => {
-    const res =  await fetch('http://localhost:3000/api/notepages');
+    const res =  await fetch('https://notebook-navy.vercel.app/api/notepages/895');
     const data = await res.json();
     const pages : Page[] = data.data;
     console.log(pages.map)
   return (
-    <div className='bg-pink-600 p-6 text-white text-xl text-center w-1/7 overflow-hidden'>
+    <div className='bg-pink-700 p-6 text-white text-xl text-center w-1/7 overflow-hidden'>
           <ul>
             {pages.map(page => 
                 <li className='m-3' key={page._id}>{page.categorie}</li> 
